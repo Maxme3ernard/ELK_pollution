5d3003852bef0e7811b4a9660ecb790fb277b119
tletutour <tom.le-tutour@insa-lyon.fr>
2020-09-22 06:38:06 -0700

    aqi is retrieved (but it is super slow)

--- request.go
+++ request.go
@@ -1,23 +1,36 @@
 package main
+
 import (
-	"net/http"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"time"
+	"net/http"
 	"strconv"
+	"time"
 )
+
 type StationsResponse struct {
-	Data []Station `json:"data"`
-	Status string `json:"status"`
+	Data   []Station `json:"data"`
+	Status string    `json:"status"`
 }
 type DataResponse struct {
-	Data []interface{} `json:"data"`
-	Status string `json:"status"`
+	Data   DataPoint `json:"data"`
+	Status string    `json:"status"`
 }
 type Geotag struct {
 	Lat float64
 	Lng float64
+}
+type DataPoint struct {
+	Aqi          int
+	Idx          int
+	Attributions []interface{}
+	city         []interface{}
+	DominentPol  string
+	iaqi         []interface{}
+	time         []interface{}
+	forecast     []interface{}
+	debug        []interface{}
 }
 type Station struct {
 	G []float64
@@ -29,13 +42,10 @@ type Station struct {
 	S string
 	Z string
 }
-type DataPoint struct {
-	
-}
 
 func main() {
-	// We fetch all stations 
-	
+	// We fetch all stations
+
 	resp, err := http.Get("https://airnet.waqi.info/airnet/map/stations")
 	if err != nil {
 		// handle error
@@ -47,33 +57,36 @@ func main() {
 	var result StationsResponse
 	json.Unmarshal(respTxt, &result)
 	var stations = result.Data
-	fmt.Printf("nb  of stations %d", len(stations))
-	token := "abe466e87b9df8832dfe2f08d96b915adbe4cdb1"
-	
-	for index, element:= range stations{
-		
+	fmt.Println("nb  of stations %d", len(stations))
+
+	for index, element := range stations {
+
 		if (index+1)%500 == 0 {
-			// We are limited to 1000 calls a second 
+			// We are limited to 1000 calls a second
 			// We stop every 500 stations and sleep for 1 second, just to be safe
-			fmt.Printf("stop cond reached %d \n",index)
-			time.Sleep(1 * time.Second)	
+			// fmt.Println("stop cond reached %d ", index)
+			time.Sleep(1 * time.Second)
 		}
-		// fmt.Printf("station n %d: %s \n", index,element.N)
-		fmt.Printf("station %d geotag (%f,%f) \n", index,element.G[0],element.G[1])
-		var requestUrl = "https://api.waqi.info/feed/geo:"+strconv.FormatFloat(element.G[0], 'E', -1, 64)+";"+strconv.FormatFloat(element.G[1], 'E', -1, 64)+"/?token="+token
-		resp, err = http.Get(requestUrl)
+		fmt.Printf("station n %d: %s ", index, element.N)
+		var requestURL = getAPIURL(element.G[0], element.G[1])
+		resp, err = http.Get(requestURL)
 		if err != nil {
 			// handle error
 			panic(err)
 		}
 		defer resp.Body.Close()
 		respTxt, err := ioutil.ReadAll(resp.Body)
-	
-		m := make(map[string]interface{})
-		err := json.Unmarshal(data, &m)
 		if err != nil {
-			log.Fatal(err)
+			// handle error
+			panic(err)
 		}
-		fmt.Println(m["id"])
+		var m DataResponse
+		json.Unmarshal(respTxt, &m)
+		fmt.Println("Air quality index : %d", m.Data.Aqi)
 	}
+}
+
+func getAPIURL(lat float64, lng float64) string {
+	token := "abe466e87b9df8832dfe2f08d96b915adbe4cdb1"
+	return "https://api.waqi.info/feed/geo:" + strconv.FormatFloat(lat, 'f', -1, 64) + ";" + strconv.FormatFloat(lng, 'f', -1, 64) + "/?token=" + token
 }
